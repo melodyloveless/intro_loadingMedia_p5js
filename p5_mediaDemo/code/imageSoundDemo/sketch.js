8d6a6b8936d2154e360910a575ace5f650f1bb93
melodyloveless <5409179+melodyloveless@users.noreply.github.com>
2021-10-04 11:13:53 -0400

    Add files via upload

---
+++ p5_mediaDemo/code/imageSoundDemo/sketch.js
@@ -0,0 +1,12 @@
+/// Image and Sound Demo
+/// Note: If you want to include sound,
+/// you need to include p5 sound library
+/// delete comments from index.html
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
